e9e893116f50d3d43051c785978e550eb0bf7ba6
Michelle Hecht <mhecht21@gmail.com>
2016-08-09 01:25:47 -0400

    Original version

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var sound;
+ 
+function preload()
+{
+  // initialize sound
+  sound = loadSound('pop.mp3');
+}
+ 
+ 
+function setup() 
+{
+  createCanvas(200, 200);
+  
+  // display instructions
+  textAlign(CENTER);
+  fill(100);
+  noStroke();
+  text("Click to play sound", width/2, height/2);
+}
+ 
+function draw()
+{
+  
+}
+ 
+function mousePressed() 
+{
+  // trigger sound
+  sound.play();
+  
+  // change background color
+  background(random(255), random(255), random(255));
+  
+}
+    
